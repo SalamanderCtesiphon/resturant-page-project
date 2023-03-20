7da47b493ebbc0837851ac0ff1230ec68c8f4553
salamanderctesiphon <swbrookshire@gmail.com>
2023-03-20 10:11:55 -0500

    added a style sheet

--- webpack.config.js
+++ webpack.config.js
@@ -10,14 +10,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader' , 'css-loader'],
-      },
-      {
-
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-
-        type: 'asset/resource',
-
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
