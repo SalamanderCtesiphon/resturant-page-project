4d94e4c3b0a9e1534bee185978c19d86365d151a
salamanderctesiphon <swbrookshire@gmail.com>
2023-03-20 15:54:27 -0500

    I have the basic website almost finished I just need to add some links to the git hub repo and clean up the text and styling a little

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -1,12 +1,27 @@
-
+import image4 from '../images/tomas-jasovsky-d5SZqLkpIrY-unsplash.jpg';
 
 const contactInfo = () => {
   const main = document.querySelector('.main');
   main.innerText= '';
-  const contactInfo = document.createElement('div');
+  const contactInfo = document.createElement('h1');
+  contactInfo.classList.add('nameOfResturant');
+
+  const contactTextContent = document.createElement('div');
+  contactTextContent.innerText = "Email: swbrookshire@gmail.com ";
+  contactTextContent.classList.add('menuContent');
+  const para1 = document.createElement('p');
+  para1.innerText = 'This webpage was created using only vanilla JavaScript. With all the navigation between the pages being rendered dynamically.'
+
+  const contactImg = new Image();
+  contactImg.src = image4;
+  contactImg.classList.add('contactImg')
+  
   contactInfo.innerText = 'Contact';
   main.classList.add('page');
   main.appendChild(contactInfo);
+  main.appendChild(contactImg);
+  main.appendChild(contactTextContent);
+  main.appendChild(para1);
 };
 
 export { contactInfo };
