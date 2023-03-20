7bc7dfbd16968ef4bd3544bcd4f26d0e451c6e15
salamanderctesiphon <swbrookshire@gmail.com>
2023-03-20 13:17:03 -0500

    added a image to the image directory and created an h1 for the name of the resturant on the homepage

--- src/modules/homepage.js
+++ src/modules/homepage.js
@@ -4,7 +4,10 @@ const homepage = () => {
   const main = document.querySelector('.main');
   main.innerText= '';
   const homePageContent = document.createElement('div');
-  homePageContent.innerText = 'Hello';
+  const nameOfResturant = document.createElement('h1');
+  nameOfResturant.innerText = "Cafe Milano";
+  nameOfResturant.classList.add('nameOfResturant');
+  homePageContent.appendChild(nameOfResturant);
   main.classList.add('page');
   main.appendChild(homePageContent);
 };
