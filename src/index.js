0f70de3741351b4e4cc3fa75f048e2704a5eae3c
salamanderctesiphon <swbrookshire@gmail.com>
2023-03-20 13:06:05 -0500

    have the basic wire frame for the entire layout finished. All that is left is to created the text content and add images and finish styling

--- src/index.js
+++ src/index.js
@@ -11,10 +11,12 @@ const home = document.createElement('div');
 const menu = document.createElement('div');
 const contact = document.createElement('div');
 const main = document.createElement('div');
+const footer = document.createElement('div');
 
 home.textContent = 'Home';
 menu.textContent = 'Menu';
 contact.textContent = 'Contact';
+footer.textContent = 'Seamus Quixote for the odin project current year';
 
 home.addEventListener('click', homepage);
 menu.addEventListener('click', menuContent);
@@ -26,6 +28,7 @@ home.classList.add('navBtn');
 menu.classList.add('navBtn');
 contact.classList.add('navBtn');
 main.classList.add('main');
+footer.classList.add('footer');
 
 
 header.appendChild(home);
@@ -33,5 +36,6 @@ header.appendChild(menu);
 header.appendChild(contact);
 content.appendChild(header);
 content.appendChild(main);
+content.appendChild(footer);
 
 homepage();
